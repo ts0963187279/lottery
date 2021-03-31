4d13ad43d81859d99d741a0e7588d2a8dc8518b3
Victor <ts0963187279@gmail.com>
2021-03-31 16:36:27 +0800

    update to ssl and add get_users and get_gifter

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ use std::io;
 use std::process;
 use futures::executor::block_on;
 use std::collections::BTreeMap;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 struct Record {
@@ -51,9 +52,33 @@ struct GiftData {
     gifter_name: String,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct CheckAuthData {
+    access_token: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+struct GetGifterData {
+    authorization: String,
+    broadcaster_id: String,
+    ids: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+struct GetUsersData {
+    authorization: String,
+    ids: Vec<String>,
+}
+
+const CLIENT_ID: &str = "dp74x753ef0ri5gopkz53a6l44a30k";
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    lottery().await;
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("server.key", SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("hibiki-rain_com.crt").unwrap();
     HttpServer::new(|| {
         App::new()
             .wrap(
@@ -63,8 +88,11 @@ async fn main() -> std::io::Result<()> {
             )
             .wrap(middleware::Compress::new(ContentEncoding::Br))
             .route("/get_winner/", web::post().to(winner))
+            .route("/check_auth/", web::post().to(check_auth))
+            .route("/get_gifter/", web::post().to(get_gifter))
+            .route("/get_users/", web::post().to(get_users))
     })
-    .bind("0.0.0.0:5566")?
+    .bind_openssl("0.0.0.0:5566", builder)?
     .run()
     .await
 }
@@ -89,6 +117,93 @@ async fn winner(_bytes: Bytes) -> Result<HttpResponse, Error> {
     }
     let res = serde_json::to_string(&winner).unwrap();
     Ok(HttpResponse::Ok().body(res))
+}
+
+async fn check_auth(_bytes: Bytes) -> Result<HttpResponse, Error> {
+    let req = String::from_utf8_lossy(&_bytes);
+    println!("check_auth req: {}", req);
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+    let data: CheckAuthData = serde_json::from_str(&req)?;
+    let twitch_uri = format!(
+        "https://id.twitch.tv/oauth2/validate"
+    );
+    let request = Request::builder()
+    .method("GET")
+    .uri(twitch_uri)
+    .header("Authorization", format!("OAuth {}", data.access_token))
+    .body(hyper::Body::empty())
+    .unwrap();
+    let res = client.request(request).await?;
+    let body = hyper::body::to_bytes(res).await?;
+    let str_body = String::from_utf8_lossy(&body);
+    let v: Value = serde_json::from_str(&str_body)?;
+    let response = v.to_string();
+    Ok(HttpResponse::Ok().body(response))
+}
+
+async fn get_gifter(_bytes: Bytes) -> Result<HttpResponse, Error> {
+    println!("get gifter req");
+    let req = String::from_utf8_lossy(&_bytes);
+    let data: GetGifterData = serde_json::from_str(&req)?;
+    let mut ids: String = format!("");
+    for id in data.ids {
+        ids = format!("{}&user_id={}",ids ,id);
+    }
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+    let steam_uri = format!(
+        "https://api.twitch.tv/helix/subscriptions?broadcaster_id={}{}",
+        data.broadcaster_id, ids
+    );
+    let request = Request::builder()
+    .method("GET")
+    .uri(steam_uri)
+    .header("Client-ID", CLIENT_ID)
+    .header("Authorization", format!("Bearer {}", data.authorization))
+    .body(hyper::Body::empty())
+    .unwrap();
+    let res = client.request(request).await?;
+    let body = hyper::body::to_bytes(res).await?;
+    let str_body = String::from_utf8_lossy(&body);
+    let v: Value = serde_json::from_str(&str_body)?;
+    let response = v.to_string();
+    Ok(HttpResponse::Ok().body(response))
+}
+
+async fn get_users(_bytes: Bytes) -> Result<HttpResponse, Error> {
+    println!("get users req");
+    let req = String::from_utf8_lossy(&_bytes);
+    let data: GetUsersData = serde_json::from_str(&req)?;
+    let mut ids: String = format!("");
+    let mut count = 0;
+    for id in data.ids {
+        count += 1;
+        ids = format!("{}&login={}",ids ,id);
+        println!("{}, {}", count, id);
+        if count == 100 {
+            break;
+        }
+    }
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+    let uri = format!(
+        "https://api.twitch.tv/helix/users?{}",
+        ids
+    );
+    let request = Request::builder()
+        .method("GET")
+        .uri(uri)
+        .header("Client-ID", CLIENT_ID)
+        .header("Authorization",format!("Bearer {}", data.authorization))
+        .body(hyper::Body::empty())
+        .unwrap();
+    let res = client.request(request).await?;
+    let body = hyper::body::to_bytes(res).await?;
+    let str_body = String::from_utf8_lossy(&body);
+    let v: Value = serde_json::from_str(&str_body)?;
+    let response = v.to_string();
+    Ok(HttpResponse::Ok().body(response))
 }
 
 fn get_winner(i: i32) -> Result<(Vec<String>), Box<AE>> {
@@ -131,31 +246,31 @@ fn get_winner(i: i32) -> Result<(Vec<String>), Box<AE>> {
     Ok(winner)
 }
 
-async fn get_gifter(user_id: String) -> Result<Value, Error> {
-    let https = HttpsConnector::new();
-    let client = Client::builder().build::<_, hyper::Body>(https);
-    let broadcaster_id = "11561802";
-    let client_id = "fsbi12mzorvkvfp9vtui6hkx4vj7hn";
-    let authorization = "Bearer drgvgtw0csccrh6zs0vyqag4tx1wqx";
-    let steam_uri = format!(
-        "https://api.twitch.tv/helix/subscriptions?broadcaster_id={}&user_id={}",
-        broadcaster_id, user_id
-    );
-    println!("{}", steam_uri);
-    let request = Request::builder()
-    .method("GET")
-    .uri(steam_uri)
-    .header("Client-ID", client_id)
-    .header("Authorization", authorization)
-    .body(hyper::Body::empty())
-    .unwrap();
-    let res = client.request(request).await?;
-    let body = hyper::body::to_bytes(res).await?;
-    let str_body = String::from_utf8_lossy(&body);
-    let v: Value = serde_json::from_str(&str_body)?;
-    let response = v.to_string();
-    Ok(v)
-}
+// async fn get_gifter(user_id: String) -> Result<Value, Error> {
+//     let https = HttpsConnector::new();
+//     let client = Client::builder().build::<_, hyper::Body>(https);
+//     let broadcaster_id = "11561802";
+//     let client_id = "fsbi12mzorvkvfp9vtui6hkx4vj7hn";
+//     let authorization = "Bearer drgvgtw0csccrh6zs0vyqag4tx1wqx";
+//     let steam_uri = format!(
+//         "https://api.twitch.tv/helix/subscriptions?broadcaster_id={}&user_id={}",
+//         broadcaster_id, user_id
+//     );
+//     println!("{}", steam_uri);
+//     let request = Request::builder()
+//     .method("GET")
+//     .uri(steam_uri)
+//     .header("Client-ID", client_id)
+//     .header("Authorization", authorization)
+//     .body(hyper::Body::empty())
+//     .unwrap();
+//     let res = client.request(request).await?;
+//     let body = hyper::body::to_bytes(res).await?;
+//     let str_body = String::from_utf8_lossy(&body);
+//     let v: Value = serde_json::from_str(&str_body)?;
+//     let response = v.to_string();
+//     Ok(v)
+// }
 
 async fn get_twitch_id(login: String) -> Result<Value, Error> {
     let https = HttpsConnector::new();
@@ -214,40 +329,44 @@ async fn lottery() -> Result<(), Box<dyn AE>> {
         let giftData: GiftData = result?;
         gifters.insert(giftData.user_id, giftData.gifter_id); 
     }
-    rdr = csv::Reader::from_path("lottery_list.csv")?;
+    println!("start parse");
+    let mut rdr = csv::Reader::from_path("lottery_list.csv")?;
     let mut lottery_pool: Vec<Record> = Vec::new();
     for result in rdr.deserialize() {
         let record: Record = result?;
-        if record.SubType == "gift" {
-            let mut v = get_twitch_id(record.Username.clone()).await?;
-            let mut id_len = v["data"][0]["id"].to_string().len();
-            let userID: String = v["data"][0]["id"].to_string()[1..id_len-1].to_string();
-            if let Some(gifterID) = gifters.get(&userID) {
-                v = get_twitch_user(gifterID.to_string()).await?;
-                let gifterName = v["data"][0]["login"].to_string();
-                println!("{}", gifterName);
-                let mut newRecord = record.clone();
-                newRecord.Username = gifterName;
-                for _ in 0..record.Tenure/3 {
-                    lottery_pool.push(newRecord.clone());
-                }
-                lottery_pool.push(newRecord.clone());
-            } else {
-                for _ in 0..record.Tenure/3 {
-                    lottery_pool.push(record.clone());
-                }
-                lottery_pool.push(record.clone());
-            }
-        }else{
+        println!("record : {:?}", record);
+        // if record.SubType == "gift" {
+        //     let mut v = get_twitch_id(record.Username.clone()).await?;
+        //     let mut id_len = v["data"][0]["id"].to_string().len();
+        //     let userID: String = v["data"][0]["id"].to_string()[1..id_len-1].to_string();
+        //     if let Some(gifterID) = gifters.get(&userID) {
+        //         v = get_twitch_user(gifterID.to_string()).await?;
+        //         let gifterName = v["data"][0]["login"].to_string();
+        //         println!("{}", gifterName);
+        //         let mut newRecord = record.clone();
+        //         newRecord.Username = gifterName;
+        //         for _ in 0..record.Tenure/3 {
+        //             lottery_pool.push(newRecord.clone());
+        //         }
+        //         lottery_pool.push(newRecord.clone());
+        //     } else {
+        //         for _ in 0..record.Tenure/3 {
+        //             lottery_pool.push(record.clone());
+        //         }
+        //         lottery_pool.push(record.clone());
+        //     }
+        // }else{
             for _ in 0..record.Tenure/3 {
                 lottery_pool.push(record.clone());
             }
             lottery_pool.push(record.clone());
-        }
+            println!("record : {:?}", record.clone());
+        // }
     }
     let mut wtr = csv::Writer::from_path("lottery_pool.csv")?;
     for u in lottery_pool {
         wtr.serialize(u)?;
     }
+    println!("done!");
     Ok(())
 }
